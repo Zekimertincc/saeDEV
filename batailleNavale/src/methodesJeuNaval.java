11b94ba130447b69ff5db3edc4c40d41c50401f7
DaichiDen <165920391+DaichiDen@users.noreply.github.com>
2024-12-13 14:57:44 +0100

    update

--- batailleNavale/src/methodesJeuNaval.java
+++ batailleNavale/src/methodesJeuNaval.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class methodesJeuNaval {
 //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
+    //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
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,26 +13,20 @@ public class methodesJeuNaval {
         numéroterPlateau(plateau);
         //saisirBateaux(plateau);
         afficherPlateau(plateau);
-        int longueur=3;
-        int[]bateau1test=new int[longueur];
 
-        bateau1test=saisirBat(longueur);
-        for (int i=0;i<longueur;i++){
-            System.out.println(bateau1test[i]);
-        }
 
     }
 
 
-
-    public static void remplirPlateauVide(String [][] plateau ) { // Cette procédure remplit un tableau 2D vide de 11*11 avec des "-"
+    public static void remplirPlateauVide(String[][] plateau) { // Cette procédure remplit un tableau 2D vide de 11*11 avec des "-"
         for (int i = 0; i < 11; i++) {
             for (int j = 0; j < 11; j++) {
                 plateau[i][j] = "- ";
             }
         }
     }
-    public static void afficherPlateau(String[][] plateau ) { // Cette procédure affiche le tableau 2D en forme de tableau (I J)
+
+    public static void afficherPlateau(String[][] plateau) { // Cette procédure affiche le tableau 2D en forme de tableau (I J)
         System.out.println("La bataille navale oppose deux joueurs. Chaque joueur dispose de deux grilles carrées de côté 10, dont les lignes sont numérotées de 1 à 10 et les colonnes de A à J, ainsi que d'une flotte composée de quelques bateaux d'une à cinq cases de long.\n" +
                 "\n" +
                 "L'une des grilles représente la zone contenant sa propre flotte. Au début du jeu, chaque joueur place ses bateaux sur sa grille, en s'assurant que deux bateaux ne sont pas adjacents. L'autre représente la zone adverse, où il cherchera à couler les bateaux de son adversaire.\n" +
@@ -41,7 +37,7 @@ public class methodesJeuNaval {
         for (int i = 0; i < 11; i++) {
 
             for (int j = 0; j < 11; j++) {
-                System.out.print(plateau[i][j] + "\t" );
+                System.out.print(plateau[i][j] + "\t");
 
             }
             System.out.println();
@@ -55,62 +51,45 @@ public class methodesJeuNaval {
         for (int i = 0; i < 11; i++) {
 
             for (int j = 0; j < 11; j++) {
-                plateau[0][j] = Integer.toString(j+1);
-                plateau[i][0] = Integer.toString(i+1);
+                plateau[0][j] = Integer.toString(j + 1);
+                plateau[i][0] = Integer.toString(i + 1);
             }
         }
     }
 
 
     public static void saisirBateaux(String[][] plateau) { //Cette procédure demande à l'utilisateur d'entrer plusieurs données:
-                                                            // -Les coordonnées X et Y d'un bateau de longueur 1
-                                                            // -Les coordonnées X et Y du début du bateau de longueur 2 ainsi que les coordonnées X et Y de la fin
-                                                            // - Idem pour longueur 3
+        // -Les coordonnées X et Y d'un bateau de longueur 1
+        // -Les coordonnées X et Y du début du bateau de longueur 2 ainsi que les coordonnées X et Y de la fin
+        // - Idem pour longueur 3
         Scanner scanner = new Scanner(System.in);
-        boolean bateauxMis = false;
-        System.out.println("");
-        int counter = 0;
-        int[] bateauxLong1 = new int[2];
-        int[] bateauxLong2 = new int[4];
-        int[] bateauxLong3 = new int[4];
-
-        while (!bateauxMis) {
-
-            System.out.println("??? regles ???");
-            System.out.println("??? donner cordinates pour bat1 ??? commence avec X et après Y");
-             bateauxLong1 = saisirBat(1);
+        int taillebat ;
+        boolean touteBienSaisie = false;
 
 
-            System.out.println("??? donner cordinates pour bat2 ???");
-
-                for (int i = 0; i < bateauxLong1.length; i++) {
-                    bateauxLong2[i] = scanner.nextInt()  ;
-                    counter++;
-                }
-
-
-            System.out.println("??? donner cordinates pour bat3 ??? User mets coordonnées X et Y du début du bateau et X et Y de la fin du bateau");
-
-                for (int i = 0; i < bateauxLong1.length; i++) {
-                    bateauxLong3[i] = scanner.nextInt()  ;
-                    counter++;
-                }
-
-
-
-            if(counter == 10 ){
-                bateauxMis = true;
+            System.out.println("quelle taille de bateaux");
+            taillebat = scanner.nextInt();
+            int bat1[]=  new int[taillebat + 1 ];
+            bat1= saisirBat(taillebat);
+            for (int i = 0; i < taillebat +1 ; i++) {
+                System.out.println(bat1[i]);
             }
-        }
-        plateau[bateauxLong1[0]][bateauxLong1[0]] = "X";
 
-        plateau[bateauxLong2[0]][bateauxLong2[1]]="X";
-        plateau[bateauxLong2[1]][bateauxLong2[2]]="X";
+            System.out.println("quelle taille de bateaux");
+            taillebat = scanner.nextInt();
+            int bat2[]= new int[taillebat +1 ];
+            bat2= saisirBat(taillebat);
+            for (int i = 0; i < bat2.length  ; i++) {
+                System.out.println(bat2[i]);
+            }
 
-
-        for (int i = 1; i < bateauxLong3.length;i++){
-            plateau[bateauxLong3[i]][bateauxLong3[i+1]]= "X";
-        }
+            System.out.println("quelle taille de bateaux");
+            taillebat = scanner.nextInt();
+            int bat3[]= new int[taillebat];
+            bat3= saisirBat(taillebat);
+            for (int i = 0; i < taillebat; i++) {
+                System.out.println(bat3[i]);
+            }
 
 
 
@@ -118,11 +97,10 @@ public class methodesJeuNaval {
 
     }
 
-    public static int[] saisirBat(int longeur){
-        int tab[] = new int[longeur];
+    public static int[] saisirBat(int longueur) {
+        int tab[] = new int[longueur + 1 ];
         Scanner scanner = new Scanner(System.in);
-        int i = 0;
-        int cpt=0;
+        int cpt = 0;
         boolean saisieCorrect = false;
 
             if (longeur>1){
@@ -164,18 +142,13 @@ public class methodesJeuNaval {
 
 
 
+        }
 
 
-        return tab;
+
     }
-
-
 
 
     //public static void mettreBateuxSurPlateaux(int[]b1, int[]b2, int[]b3,String[][] plateau) {
 
 
-    //}
-
-
-}
