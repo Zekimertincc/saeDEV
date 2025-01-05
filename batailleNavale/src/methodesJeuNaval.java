7654df4c2570905deaa86a5a59d12ed1f6539332
Zekimertincc <Zekimertinc@gmail.com>
2025-01-05 15:39:19 +0100

    ..

--- batailleNavale/src/methodesJeuNaval.java
+++ batailleNavale/src/methodesJeuNaval.java
@@ -196,6 +196,7 @@ public class methodesJeuNaval {
         numéroterPlateau(plateau);
         numéroterPlateau(plateauOrdi);
         saisirBateauxPlateauOrdi(plateauOrdi);
+        ajouterSurPlateau(plateau);
 
 
 
