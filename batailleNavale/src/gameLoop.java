e4600a9011382f4147c3385284abef894e55e73c
zeki <zekimertinc@gmail.com>
2025-01-05 21:16:55 +0100

    derniere truc a regarder

--- batailleNavale/src/gameLoop.java
+++ batailleNavale/src/gameLoop.java
@@ -5,26 +5,46 @@ public class gameLoop {
         String[][] plateau = new String[11][11];
         String[][]plateauOrdi = new String[11][11];
 
-        int counter  = 0;
-        int counter2  = 0;
+        int choix=0;
+
+        methodesJeuNaval.setupMenus();
         methodesJeuNaval.setupJeux(plateau, plateauOrdi);
+        System.out.println("\n");
+        System.out.println("La bataille navale oppose deux joueurs. Chaque joueur dispose de deux grilles carrées de côté 10, dont les lignes sont numérotées de 1 à 10 et les colonnes de A à J, ainsi que d'une flotte composée de quelques bateaux d'une à cinq cases de long.\n" +
+                "\n" +
+                "L'une des grilles représente la zone contenant sa propre flotte. Au début du jeu, chaque joueur place ses bateaux sur sa grille, en s'assurant que deux bateaux ne sont pas adjacents. L'autre représente la zone adverse, où il cherchera à couler les bateaux de son adversaire.\n" +
+                "\n" +
+                "Chaque joueur, à son tour, annonce une case (par exemple « B6 »), et son adversaire lui répond si le tir tombe à l'eau ou au contraire s'il touche un bateau. Dans ce dernier cas, il annonce « touché » s'il reste des cases intactes au bateau ciblé, et « touché-coulé » si non6.\n" +
+                "\n" +
+                "Diverses variantes existent, par exemple le fait de tirer les coups par salves et de ne donner que le résultat global de la salve. ");
+        methodesJeuNaval.afficherPlateau(plateau);
 
         methodesJeuNaval.afficherPlateau(plateauOrdi);
+
         do {
 
-            methodesJeuNaval.attackPlayer(plateau);
+            methodesJeuNaval.attackOrdi(plateauOrdi);
             if (tousLesBatCoules(plateauOrdi)) {
-                System.out.println("Vous avez gagne!");
+                System.out.println("Vous avez gagné!");
                 gameOver = true;
                 break;
             }
-            methodesJeuNaval.attackOrdi(plateauOrdi);
+            if (choix == 1 ){
+                methodesJeuNaval.attackPlayer(plateau);
+            }
+            if (choix == 2){
+
+
+                methodesJeuNaval.attackPlayer2(plateau);
+            }
+
             if (tousLesBatCoules(plateau)) {
                 System.out.println("Vous avez perdu!");
                 gameOver = true;
                 break;
             }
             methodesJeuNaval.afficherPlateau(plateauOrdi);
+            methodesJeuNaval.afficherPlateau(plateau);
         }while (!gameOver);
     }
 
--- batailleNavale/src/methodesJeuNaval.java
+++ batailleNavale/src/methodesJeuNaval.java
@@ -7,16 +7,36 @@ import java.util.ArrayList;
 public class methodesJeuNaval {
 //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
 
-//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 TOKEN DENIS
+
+
+    //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 TOKEN DENIS
     public static void main(String[] args) {
+
 
         String[][] plateau = new String[11][11];
         String[][]plateauOrdi = new String[11][11];
 
-        setupJeux(plateau, plateauOrdi);
-        attackPlayer(plateau);
-        afficherPlateau(plateau);
-        methodesUtilisateur.ajouterSurPlateau(plateau);
+        methodesJeuNaval.remplirPlateauVide(plateau);
+        methodesJeuNaval.numéroterPlateau(plateau);
+
+
+        //setupJeux(plateau, plateauOrdi);
+
+        ArrayList<Integer> tabtestFaux = new ArrayList<>();
+        tabtestFaux.add(1);
+        tabtestFaux.add(10);
+        tabtestFaux.add(3);
+        System.out.println(peutEtrePlacé(tabtestFaux,plateau,3));
+
+        ArrayList<Integer> tabtestVrai = new ArrayList<>();
+        tabtestVrai.add(5);
+        tabtestVrai.add(5);
+        tabtestVrai.add(1);
+        System.out.println(peutEtrePlacé(tabtestVrai,plateau,1));
+
+        //attackPlayer(plateau);
+        //afficherPlateau(plateau);
+        //ajouterSurPlateau(plateau);
 
     }
 
@@ -29,13 +49,7 @@ public class methodesJeuNaval {
         }
     }
     public static void afficherPlateau(String[][] plateau) { // Cette procédure affiche le tableau 2D en forme de tableau (I J)
-        System.out.println("La bataille navale oppose deux joueurs. Chaque joueur dispose de deux grilles carrées de côté 10, dont les lignes sont numérotées de 1 à 10 et les colonnes de A à J, ainsi que d'une flotte composée de quelques bateaux d'une à cinq cases de long.\n" +
-                "\n" +
-                "L'une des grilles représente la zone contenant sa propre flotte. Au début du jeu, chaque joueur place ses bateaux sur sa grille, en s'assurant que deux bateaux ne sont pas adjacents. L'autre représente la zone adverse, où il cherchera à couler les bateaux de son adversaire.\n" +
-                "\n" +
-                "Chaque joueur, à son tour, annonce une case (par exemple « B6 »), et son adversaire lui répond si le tir tombe à l'eau ou au contraire s'il touche un bateau. Dans ce dernier cas, il annonce « touché » s'il reste des cases intactes au bateau ciblé, et « touché-coulé » si non6.\n" +
-                "\n" +
-                "Diverses variantes existent, par exemple le fait de tirer les coups par salves et de ne donner que le résultat global de la salve. ");
+
         for (int i = 0; i < 11; i++) {
 
             for (int j = 0; j < 11; j++) {
@@ -63,31 +77,33 @@ public class methodesJeuNaval {
         int[] directionsX = {-1, 1, 0, 0};
         int[] directionsY = {0, 0, 1, -1};
 
-        for (int i = 1; i <= 3; i++) {
-            ArrayList<Integer> bateau = saisirBat(i);
-            boolean peutPlacer = peutEtrePlacé(bateau, plateau, i);
+        for (int i = 1; i <= 6; i++) {
+            while(true) {
+                ArrayList<Integer> bateau = saisirBat(i);
+                boolean peutPlacer = peutEtrePlacé(bateau, plateau, i);
 
-            if (peutPlacer) {
-                int x = bateau.get(0);
-                int y = bateau.get(1);
+                if (peutPlacer) {
+                    int x = bateau.get(0);
+                    int y = bateau.get(1);
 
-                plateau[x][y] = "X";
+                    plateau[x][y] = "X";
 
-                if (i > 1 && peutPlacer) {
-                    int dir = bateau.get(2);
-                    for (int j = 1; j < i; j++) {
-                        int newX = x + (j * directionsX[dir - 1]);
-                        int newY = y + (j * directionsY[dir - 1]);
-                        if (newX >= 0 && newX < plateau.length && newY >= 0 && newY < plateau[0].length) {
-                            plateau[newX][newY] = "X";
+                    if (i > 1 && peutPlacer) {
+                        int dir = bateau.get(2);
+                        for (int j = 1; j < i; j++) {
+                            int newX = x + (j * directionsX[dir - 1]);
+                            int newY = y + (j * directionsY[dir - 1]);
+                            if (newX >= 0 && newX < plateau.length && newY >= 0 && newY < plateau[0].length) {
+                                plateau[newX][newY] = "X";
+                            }
                         }
+
+
                     }
-
-
-
+                    break;
+                } else {
+                    System.out.println("T'as mal saisi le bateau" + i);
                 }
-            } else {
-                System.out.println("mal saise le bateaux." + i);
             }
         }
     }
@@ -97,58 +113,62 @@ public class methodesJeuNaval {
         int counter = 0;
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> tab = new ArrayList<Integer>();
-        System.out.println("votre bateaux a  du taille du " + longueur );
-            if (longueur>1){
+        System.out.println("Le batteau à mettre est de taille : " + longueur );
+        if (longueur>1){
 
-                do{
-                    System.out.println("donner x et y apres");
-                    for (int i = 0; i < 2 ; i++) {
-                        saisie = scanner.nextInt();
-                        tab.add(saisie);
-                        if ( saisie<= 10 && saisie>= 1 ){
-                            counter++;
-                        }
-                    }
-                    System.out.println("saisir la direction de la façon suivante : 1 pour nord 2 pour sud 3 pour est et 4 pour ouest");
+            do {
+                counter=0;
+                tab.clear();
+                System.out.println("donner y et après x");
+                for (int i = 0; i < 2; i++) {
                     saisie = scanner.nextInt();
                     tab.add(saisie);
-                    if (saisie<= 4 && saisie>= 1 ){
+                    if (saisie <= 10 && saisie >= 1) {
                         counter++;
                     }
-                    if (counter == 3){
-                        saisieCorrecte = true;
+                }
+
+                System.out.println("saisir la direction de la façon suivante : 1 pour nord 2 pour sud 3 pour est et 4 pour ouest");
+                saisie = scanner.nextInt();
+                tab.add(saisie);
+                if (saisie <= 4 && saisie >= 1) {
+                    counter++;
+                }
+                if (counter == 3) {
+                    saisieCorrecte = true;
+                }
+
+
+            }while (!saisieCorrecte);
+
+
+
+        }
+        else{
+
+            do{
+                System.out.println("donner y et après x");
+                for (int i = 0; i < 2 ; i++) {
+                    saisie = scanner.nextInt();
+                    tab.add(saisie);
+                    if ( saisie<= 10 && saisie>= 1 ){
+                        counter++;
                     }
+                }
 
-                }while (!saisieCorrecte);
+                if (counter == 2){
+                    saisieCorrecte = true;
+                }
 
-
-
-            }
-            else{
-
-                do{
-                    System.out.println("donner x et y apres");
-                    for (int i = 0; i < 2 ; i++) {
-                        saisie = scanner.nextInt();
-                        tab.add(saisie);
-                        if ( saisie<= 10 && saisie>= 1 ){
-                            counter++;
-                        }
-                    }
-
-                    if (counter == 2){
-                        saisieCorrecte = true;
-                    }
-
-                }while (!saisieCorrecte);
+            }while (!saisieCorrecte);
 
 
-            }
+        }
 
         return tab;
 
 
-        }
+    }
     public static boolean peutEtrePlacé(ArrayList<Integer> tab, String[][] plateau, int l ) {
         boolean peutplacer = true;
         int x = tab.get(0);
@@ -162,20 +182,21 @@ public class methodesJeuNaval {
             if (dir == 1) {
                 newX -= i;
             } else if (dir == 2) {
-                    newX += i;
+                newX += i;
             } else if (dir == 3) {
-                    newY += i;
+                newY += i;
             } else if (dir == 4) {
-                    newY -= i;
+
+                newY -= i;
             }
             // controle de bordures ( il regarde si c'est bien sur le plateaux)
             if (newX < 0 || newX >= plateau.length || newY < 0 || newY >= plateau[0].length) {
-                    return false;
+                return false;
             }
 
             // regarde si il y a des bateaux qui se croisent (overlap )
             if (!plateau[newX][newY].equals("- ")) {
-                    return false;
+                return false;
             }
 
 
@@ -191,6 +212,8 @@ public class methodesJeuNaval {
 
     }
     public static void setupJeux(String[][] plateau,String[][]plateauOrdi){
+
+
         remplirPlateauVide(plateau);
         remplirPlateauVide(plateauOrdi);
         numéroterPlateau(plateau);
@@ -209,15 +232,15 @@ public class methodesJeuNaval {
         int[] directionsX = {-1, 1, 0, 0};
         int[] directionsY = {0, 0, 1, -1};
 
-        for (int i = 1; i < 4; i++) { // Loop for ships of lengths 1, 2, and 3
+        for (int i = 1; i <= 6; i++) {
             boolean placed = false;
 
             while (!placed) {
                 int randX = (int) (Math.random() * range) + min;
                 int randY = (int) (Math.random() * range) + min;
-                int randDir = (i > 1) ? (int) (Math.random() * 4) + 1 : 0; // Random direction for ships longer than 1
+                int randDir = (i > 1) ? (int) (Math.random() * 4) + 1 : 0; // ici le direction en random
 
-                // Create an ArrayList to simulate user input for peutEtrePlacé
+
                 ArrayList<Integer> bateau = new ArrayList<>();
                 bateau.add(randX);
                 bateau.add(randY);
@@ -225,7 +248,7 @@ public class methodesJeuNaval {
                     bateau.add(randDir);
                 }
 
-                // Check if the ship can be placed
+                // regarder si le bateaux peut etre place
                 if (peutEtrePlacé(bateau, plateauOrdi, i)) {
                     // Place the ship
                     plateauOrdi[randX][randY] = "X";
@@ -238,7 +261,7 @@ public class methodesJeuNaval {
                         }
                     }
 
-                    placed = true; // Ship successfully placed
+                    placed = true;
                 }
             }
         }
@@ -249,17 +272,17 @@ public class methodesJeuNaval {
         Scanner scanner = new Scanner(System.in);
         int saisieX;
         int saisieY;
-        System.out.println("hangi kareye saldırcan onu seç önce x sonra y");
+        System.out.println("Saisissez les coordonnées pour frapper un bateau Y et après X");
         saisieX = scanner.nextInt();
         saisieY = scanner.nextInt();
 
         if ((saisieX <=10 && saisieX >=1) && (saisieY <=10 && saisieY >=1) ){
             if (plateauOrdi[saisieX][saisieY].equals("X")){
-                System.out.println("t as bien trouve un bateaux de ton ennemi");
+                System.out.println("tu as bien trouvé un bateau ennemi");
                 plateauOrdi[saisieX][saisieY] = "-";
             }
             else{
-                System.out.println("il y a pas de bateau la bas");
+                System.out.println("il n'y a pas de bateau à ces coordonnées");
             }
 
 
@@ -287,50 +310,157 @@ public class methodesJeuNaval {
 
 
     }
+    public static void attackPlayer2(String[][] plateau) {
+        int size = plateau.length;
 
 
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
 
+                if (plateau[y][x].equals("- ")) {
+                    attackCell(plateau, x, y);
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+        }
     }
 
 
+    public static void attackCell(String[][] plateau, int x, int y) {
+        if (plateau[y][x].equals("X")) {
+            plateau[y][x] = "H";
+            System.out.println("Vurdu! (" + x + ", " + y + ")");
+
+
+            attackSurrounding(plateau, x, y);
+        } else if (plateau[y][x].equals("- ")) {
+            plateau[y][x] = "M";
+            System.out.println("Iska! (" + x + ", " + y + ")");
+        }
+    }
+
+
+    public static void attackSurrounding(String[][] plateau, int x, int y) {
+
+        if (x + 1 < plateau.length && plateau[y][x + 1].equals("- ")) {
+            attackCell(plateau, x + 1, y); // right
+        }
+        if (x - 1 >= 0 && plateau[y][x - 1].equals("- ")) {
+            attackCell(plateau, x - 1, y); // left
+        }
+        if (y + 1 < plateau.length && plateau[y + 1][x].equals("- ")) {
+            attackCell(plateau, x, y + 1); // down
+        }
+        if (y - 1 >= 0 && plateau[y - 1][x].equals("- ")) {
+            attackCell(plateau, x, y - 1); // up
+        }
+    }
+
+
+    public static void menuPrincipal(){
+
+        System.out.println("┌────────────────────────────────────────────────┐                                 \n" +
+                "│                                                │                                 \n" +
+                "│   BIENVENUE SUR LE JEU DE LA BATAILLE NAVALE   │                                 \n" +
+                "│      ┌──────────────────────────────────┐      │                                 \n" +
+                "│      │APPUYEZ SUR ENTREE POUR COMMENCER!│      │                                 \n" +
+                "│      └──────────────────────────────────┘      │                                 \n" +
+                "│                                                │                                 \n" +
+                "│                                                │                                 \n" +
+                "│                                                │                                 \n" +
+                "│                                                │                                 \n" +
+                "│                                                │                                \n" +
+                "│                                                │                                 \n" +
+                "└────────────────────────────────────────────────┘                                 ");
+
+
+    }
+    public static void menuDifficultee(){
+        System.out.println("┌────────────────────────────────────────────────┐                                 \n" +
+                "│                                                │                                 \n" +
+                "│            CHOIX DE LA DIFFICULTE              │                                 \n" +
+                "│      ┌──────────────────────────────────┐      │                                 \n" +
+                "│      │APPUYEZ SUR :                     │      │                                 \n" +
+                "│      │                                  │      │                                 \n" +
+                "│      │  1.POUR LA DIFFICULTE FACILE     │      │                                 \n" +
+                "│      │  2.POUR LA DIFFICULTE DURE       │      │                                 \n" +
+                "│      │                                  │      │                                 \n" +
+                "│      │                                  │      │                                 \n" +
+                "│      └──────────────────────────────────┘      │                                \n" +
+                "│                                                │                                 \n" +
+                "└────────────────────────────────────────────────┘                                 ");
+    }
+    public static int setupMenus(){
+        Scanner sc = new Scanner(System.in);
+        int choixUser=-1;
+        while (true) {
+            menuPrincipal();
+            String aAppuyé = sc.nextLine();
+            if (aAppuyé.isEmpty()) {
+                break;
+            }
+        }
+        while (true) {
+            menuDifficultee();
+            if (sc.hasNextInt()) {
+                choixUser = sc.nextInt();
+                sc.nextLine();
+                if (choixUser >= 1 || choixUser <= 2) {
+                    break;
+                }
+            } else {
+                sc.nextLine();
+                System.out.println("Entrée invalide. Veuillez entrer un nombre entre 1 et 2.");
+            }
+        }
+
+        return choixUser;
+    }
+
+}
 
 
 
 
 
-    //public static void mettreBateuxSurPlateaux(int[]b1, int[]b2, int[]b3,String[][] plateau) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
